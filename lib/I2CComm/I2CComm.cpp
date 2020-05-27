ead1d44060e04b838f44deca4373e92508ebf614
Miguel Agueda <40192229+MiguelAgueda@users.noreply.github.com>
2020-05-27 14:49:21 -0700

    Update I2C to match master device.

--- lib/I2CComm/I2CComm.cpp
+++ lib/I2CComm/I2CComm.cpp
@@ -1,14 +1,8 @@
 #include <I2CComm.h>
 
 const int ADDR = 0x04; // Address for this device over I2C.
-
-void setup_i2c()
-{
-  Serial.begin(9600);
-  Wire.begin(ADDR);
-  Wire.onReceive(readI2CData);
-  Wire.onRequest(returnI2CData);
-}
+I2C_IN_PACK GLOBAL_IN_PACK;
+I2C_OUT_PACK GLOBAL_OUT_PACK;
 
 void readI2CData(int numBytes)
 {
@@ -25,9 +19,24 @@ void readI2CData(int numBytes)
 
 void returnI2CData()
 {
-  Wire.write(GLOBAL_OUT_PACK.DriveAutonomously);
-  Wire.write(GLOBAL_OUT_PACK.SaveScanData);
-  Wire.write(GLOBAL_OUT_PACK.Shutdown);
-  Wire.write(GLOBAL_OUT_PACK.joystick_x);
-  Wire.write(GLOBAL_OUT_PACK.joystick_y);
+  // int8_t buf_to_write[5] = {0};
+  // buf_to_write[0] = GLOBAL_OUT_PACK.DriveAutonomously;
+  // buf_to_write[1] = GLOBAL_OUT_PACK.SaveScanData;
+  // buf_to_write[2] = GLOBAL_OUT_PACK.Shutdown;
+  // buf_to_write[3] = GLOBAL_OUT_PACK.joystick_x;
+  // buf_to_write[4] = GLOBAL_OUT_PACK.joystick_y;
+  Wire.write((byte)GLOBAL_OUT_PACK.DriveAutonomously);
+  Wire.write((byte)GLOBAL_OUT_PACK.SaveScanData);
+  Wire.write((byte)GLOBAL_OUT_PACK.Shutdown);
+  Wire.write((int)GLOBAL_OUT_PACK.joystick_x);
+  Wire.write((byte)GLOBAL_OUT_PACK.joystick_y);
+  // Wire.write((byte)127);
+}
+
+void setup_i2c()
+{
+  Serial.begin(9600);
+  Wire.begin(ADDR);
+  Wire.onReceive(readI2CData);
+  Wire.onRequest(returnI2CData);
 }
--- lib/I2CComm/I2CComm.h
+++ lib/I2CComm/I2CComm.h
@@ -6,15 +6,17 @@ struct I2C_OUT_PACK
   bool DriveAutonomously = false;
   bool SaveScanData = false;
   bool Shutdown = false;
-  int8_t joystick_x = 0;
-  int8_t joystick_y = 0;
+  uint8_t joystick_x = 0;
+  uint8_t joystick_y = 0;
 };
 
 struct I2C_IN_PACK
 {
-  int8_t virt_joystick_x = 0;
-  int8_t virt_joystick_y = 0;
+  uint8_t virt_joystick_x = 0;
+  uint8_t virt_joystick_y = 0;
 };
 
 extern I2C_IN_PACK GLOBAL_IN_PACK;
 extern I2C_OUT_PACK GLOBAL_OUT_PACK;
+
+void setup_i2c();
